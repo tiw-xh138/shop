84a1f668b2959b673b3f2bc31dd8806859ae28a1
tiw-xh138 <facan346999e@126.com>
2018-04-21 19:42:58 +0800

    更新 items.sql

--- items.sql
+++ items.sql
@@ -10,16 +10,16 @@ create table items
   picture varchar(500) default NULL
 )
 
-
-INSERT INTO items VALUES (1, '��������Ь', '��ɽ', 180, 500, '001.jpg');
-INSERT INTO items VALUES (2, '��̤�˶�Ь', '����', 120, 800, '002.jpg');
-INSERT INTO items VALUES (3, '�Ϳ��˶�Ь', '����', 500, 1000, '003.jpg');
-INSERT INTO items VALUES (4, '���ϴ�˹TѪ��', '�Ϻ�', 388, 600, '004.jpg');
-INSERT INTO items VALUES (5, '�����Ļ���', '����', 180, 900, '005.jpg');
-INSERT INTO items VALUES (6, 'С��3', '����', 1999, 3000, '006.jpg');
-INSERT INTO items VALUES (7, 'С��2S', '����', 1299, 1000, '007.jpg');
-INSERT INTO items VALUES (8, 'thinkpad�ʼǱ�', '����', 6999, 500, '008.jpg');
-INSERT INTO items VALUES (9, 'dell�ʼǱ�', '����', 3999, 500, '009.jpg');
-INSERT INTO items VALUES (10, 'ipad5', '����', 5999, 500, '010.jpg');
+//商品信息插入存储
+INSERT INTO items VALUES (1, '沃特篮球鞋', '佛山', 180, 500, '001.jpg');
+INSERT INTO items VALUES (2, '安踏运动鞋', '福州', 120, 800, '002.jpg');
+INSERT INTO items VALUES (3, '耐克运动鞋', '广州', 500, 1000, '003.jpg');
+INSERT INTO items VALUES (4, '阿迪达斯T血衫', '上海', 388, 600, '004.jpg');
+INSERT INTO items VALUES (5, '李宁文化衫', '广州', 180, 900, '005.jpg');
+INSERT INTO items VALUES (6, '小米3', '北京', 1999, 3000, '006.jpg');
+INSERT INTO items VALUES (7, '小米2S', '北京', 1299, 1000, '007.jpg');
+INSERT INTO items VALUES (8, 'thinkpad笔记本', '北京', 6999, 500, '008.jpg');
+INSERT INTO items VALUES (9, 'dell笔记本', '北京', 3999, 500, '009.jpg');
+INSERT INTO items VALUES (10, 'ipad5', '北京', 5999, 500, '010.jpg');
 
 select * from items
