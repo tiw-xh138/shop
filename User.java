e80881eb72ac7f59305d91ea2a05923916e739b8
tiw-xh138 <facan346999e@126.com>
2018-05-20 15:19:58 +0800

    订单及用户资料的类

---
+++ User.java
@@ -0,0 +1,44 @@
+package user;
+
+public class User {
+
+	private String name;//姓名
+	private String phone;//手机号
+	private String add;//收货地址
+	private int id;//商品编号
+	private int number;//购买数量
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAdd() {
+		return add;
+	}
+	public void setAdd(String add) {
+		this.add = add;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+	
+	
+}
